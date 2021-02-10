86714574f2875131d4628a26413f43cc2a3cae07
Ababkiewicz <A.Babkiewicz@wit.edu.pl>
2021-02-10 13:36:55 +0100

    GameFlowDesign 1) Created game flow mechanics which allows to switch between game states.

---
+++ SchoolOfMagic/Assets/GameFlowScript.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameFlowScript : MonoBehaviour
+{
+    [SerializeField] Text storyText;
+    [SerializeField] Text choice1;
+    [SerializeField] Text choice2;
+    [SerializeField] Text choice3;
+    [SerializeField] Text choice4;
+    [SerializeField] State startingState;
+
+    State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        storyText.text = state.getStateStory();
+        choice1.text = state.getChoice1();
+        choice2.text = state.getChoice2();
+        choice3.text = state.getChoice3();
+        choice4.text = state.getChoice4();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+        ManageChoices();
+    }
+
+    private void ManageState()
+    {
+        State[] nextStates = state.getNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            state = nextStates[3];
+        }
+        storyText.text = state.getStateStory();
+    }
+
+    private void ManageChoices()
+    {
+        choice1.text = state.getChoice1();
+        choice2.text = state.getChoice2();
+        choice3.text = state.getChoice3();
+        choice4.text = state.getChoice4();
+    }
+}
---
+++ SchoolOfMagic/Assets/State.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+   [TextArea(14,10)] [SerializeField] string storyText;
+    [SerializeField] string choice1;
+    [SerializeField] string choice2;
+    [SerializeField] string choice3;
+    [SerializeField] string choice4;
+    [SerializeField] State[] nextStates;
+
+    public string getStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] getNextStates()
+    {
+        return nextStates;
+    }
+
+    public string getChoice1()
+    {
+        return choice1;
+    }
+
+    public string getChoice2()
+    {
+        return choice2;
+    }
+
+    public string getChoice3()
+    {
+        return choice3;
+    }
+
+    public string getChoice4()
+    {
+        return choice4;
+    }
+
+}
